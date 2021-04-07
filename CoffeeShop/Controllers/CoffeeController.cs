4448cf407caf555c203c9ef4973d556447fee90d
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-04-07 18:09:17 -0500

    Added all methods to CoffeeController

---
+++ CoffeeShop/Controllers/CoffeeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CoffeeShop.Models;
+using CoffeeShop.Repositories;
+
+namespace CoffeeShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoffeeController : ControllerBase
+    {
+        private readonly ICoffeeRepository _coffeeRepository;
+
+        public CoffeeController(ICoffeeRepository coffeeRepository)
+        {
+            _coffeeRepository = coffeeRepository;
+
+        }
+        // https://localhost:5001/api/coffee/
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_coffeeRepository.GetAll());
+        }
+
+        // https://localhost:5001/api/coffee/5
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_coffeeRepository.GetById(id));
+        }
+
+        // https://localhost:5001/api/coffee
+        [HttpPost]
+        public IActionResult Post(Coffee coffee)
+        {
+            _coffeeRepository.Add(coffee);
+            return CreatedAtAction("Get", new { id = coffee.Id }, coffee);
+        }
+
+        // https://localhost:5001/api/coffee/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Coffee coffee)
+        {
+            if(id != coffee.Id)
+            {
+                return BadRequest();
+            }
+            _coffeeRepository.Update(coffee);
+            return NoContent();
+        }
+
+        // https://localhost:5001/api/coffee/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _coffeeRepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
