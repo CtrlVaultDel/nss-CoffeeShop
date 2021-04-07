c296b9683dad9251a82f034cff4387e78cdf9419
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-04-07 17:29:58 -0500

    Added GetALL() method to CoffeeRepository

---
+++ CoffeeShop/Repositories/CoffeeRepository.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Repositories
+{
+    public class CoffeeRepository : ICoffeeRepository
+    {
+        private readonly string _connectionString;
+        public CoffeeRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        private SqlConnection Connection
+        {
+            get { return new SqlConnection(_connectionString); }
+        }
+
+        public List<Coffee> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT c.Id AS CoffeeId, c.Title, bv.Id As BeanVarietyId, bv.Name, bv.Region, bv.Notes FROM Coffee c JOIN BeanVariety bv ON c.BeanVarietyId = bv.Id";
+                    var reader = cmd.ExecuteReader();
+
+                    // Create a List to hold the Coffee Objects
+                    var allCoffee = new List<Coffee>();
+
+                    while (reader.Read())
+                    {
+                        var coffee = new Coffee()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("CoffeeId")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            BeanVarietyId = reader.GetInt32(reader.GetOrdinal("BeanVarietyId")),
+                            BeanVariety = new BeanVariety()
+                            {
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Region = reader.GetString(reader.GetOrdinal("Region"))
+                            }
+                        };
+                        if (!reader.IsDBNull(reader.GetOrdinal("Notes")))
+                        {
+                            coffee.BeanVariety.Notes = reader.GetString(reader.GetOrdinal("Notes"));
+                        }
+                        allCoffee.Add(coffee);
+                    }
+                    reader.Close();
+                    return allCoffee;
+                }
+            }
+        }                      
+
+    public Coffee GetById(int coffeeId)
+        {
+
+        }
+
+        public void Add(Coffee coffee)
+        {
+
+        }
+
+        public void Update(Coffee coffee)
+        {
+
+        }
+
+        public void Delete(int coffeeId)
+        {
+
+        }
+    }
+}
---
+++ CoffeeShop/Repositories/ICoffeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using CoffeeShop.Models;
+using System.Collections.Generic;
+
+namespace CoffeeShop.Repositories
+{
+    public interface ICoffeeRepository
+    {
+        void Add(Coffee coffee);
+        void Delete(int coffeeId);
+        List<Coffee> GetAll();
+        Coffee GetById(int coffeeId);
+        void Update(Coffee coffee);
+    }
+}
