7385538e566bbdd107d17db4790a148f65774182
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-04-07 17:39:30 -0500

    Added GetById to CoffeeRepository

--- CoffeeShop/Repositories/CoffeeRepository.cs
+++ CoffeeShop/Repositories/CoffeeRepository.cs
@@ -26,7 +26,10 @@ namespace CoffeeShop.Repositories
                 conn.Open();
                 using (var cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = "SELECT c.Id AS CoffeeId, c.Title, bv.Id As BeanVarietyId, bv.Name, bv.Region, bv.Notes FROM Coffee c JOIN BeanVariety bv ON c.BeanVarietyId = bv.Id";
+                    cmd.CommandText = @"SELECT c.Id AS CoffeeId, c.Title, bv.Id As BeanVarietyId, bv.Name, bv.Region, bv.Notes
+                                        FROM Coffee c 
+                                        JOIN BeanVariety bv 
+                                        ON c.BeanVarietyId = bv.Id";
                     var reader = cmd.ExecuteReader();
 
                     // Create a List to hold the Coffee Objects
@@ -59,22 +62,66 @@ namespace CoffeeShop.Repositories
 
     public Coffee GetById(int coffeeId)
         {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT c.Id AS CoffeeId, c.Title, bv.Id As BeanVarietyId, bv.Name, bv.Region, bv.Notes
+                                        FROM Coffee c 
+                                        JOIN BeanVariety bv 
+                                        ON c.BeanVarietyId = bv.Id
+                                        WHERE c.Id = @coffeeId";
+                    cmd.Parameters.AddWithValue("@coffeeId", coffeeId);
+                    var reader = cmd.ExecuteReader();
 
+                    Coffee coffee = null;
+                    if (reader.Read())
+                    {
+                        coffee = new Coffee()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("CoffeeId")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            BeanVarietyId = reader.GetInt32(reader.GetOrdinal("BeanVarietyId")),
+                            BeanVariety = new BeanVariety()
+                            {
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Region = reader.GetString(reader.GetOrdinal("Region"))
+                            }
+                        };
+                        if (!reader.IsDBNull(reader.GetOrdinal("Notes")))
+                        {
+                            coffee.BeanVariety.Notes = reader.GetString(reader.GetOrdinal("Notes"));
+                        }          
+                    }
+                    reader.Close();
+                    return coffee;
+                }
+            }
         }
 
         public void Add(Coffee coffee)
         {
+            using (var conn = Connection)
+            {
 
+            }
         }
 
         public void Update(Coffee coffee)
         {
+            using (var conn = Connection)
+            {
 
+            }
         }
 
         public void Delete(int coffeeId)
         {
+            using (var conn = Connection)
+            {
 
+            }
         }
     }
 }
