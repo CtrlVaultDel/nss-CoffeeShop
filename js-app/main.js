bf087da9eedc34cd517f4fe1168810fe40e4d8d7
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-04-07 19:28:04 -0500

    Added form; still unfunctional but appears on browser

--- js-app/main.js
+++ js-app/main.js
@@ -1,21 +1,28 @@
-const url = "https://localhost:5001/api/beanvariety/";
-const target = document.querySelector('.bean_container');
+const beanUrl = "https://localhost:5001/api/beanvariety/";
+const coffeeUrl = "https://localhost:5001/api/coffee/";
 
+// Document Targets
+const beanTarget = document.querySelector('.bean_container');
+const formTarget = document.querySelector('.form_container');
+const coffeeTarget = document.querySelector('.coffee_container');
+
+// Add an event listener for the initial button
 const runButton = document.querySelector("#run-button");
 runButton.addEventListener("click", () => {
     getAllBeanVarieties()
         .then(beanVarieties => {
-            target.innerHTML = listBeanVarieties(beanVarieties);
+            beanTarget.innerHTML = listBeanVarieties(beanVarieties);
         })
 });
 
+// Add an event listener onto the button that will allow the user to add a new coffee bean
 const addBeanButton = document.querySelector("#add-bean-button");
 addBeanButton.addEventListener("click", () => {
-    
+    formTarget.innerHTML = beanInputForm();
 })
 
 // Gets all Bean Varieties from the server
-const getAllBeanVarieties = () => fetch(url).then(resp => resp.json());
+const getAllBeanVarieties = () => fetch(beanUrl).then(resp => resp.json());
 
 // Separates the array of bean varieties and return a list of HTML
 const listBeanVarieties = (beanVarieties) => {
@@ -32,9 +39,36 @@ const listBeanVarieties = (beanVarieties) => {
     return beanString;
 }
 
+// Verifies if a note is empty. Returns "N/A" if it is
 const verifyNote = (note) => {
     if(note !== null){
         return note;
     }
     return "N/A";
+}
+
+const beanInputForm = () => {
+    return `
+    <form>
+        <fieldset>
+            <div>
+                <label for="name">Bean Name: <abbr title="required" aria-label="required">*</abbr></label>
+                <input id="name" type="text" name="name">
+            </div>
+        </fieldset>
+        <fieldset>
+            <div>
+                <label for="region">Bean Region: <abbr title="required" aria-label="required">*</abbr></label>
+                <input id="region" type="text" name="region">
+            </div>
+        </fieldset>
+        <fieldset>
+            <div>
+                <label for="notes">Bean Notes: </label>
+                <input id="notes" type="textbox" name="notes">
+            </div>
+        </fieldset>
+        <button type ="submit">Add Bean</button>
+    </form>
+    `
 }
