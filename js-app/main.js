c39fbaeea245b1496549acfa49e4920a3fffeec8
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-04-07 19:10:07 -0500

    Beans now appear on the DOM when the user presses the relevant button

--- js-app/main.js
+++ js-app/main.js
@@ -1,13 +1,40 @@
 const url = "https://localhost:5001/api/beanvariety/";
+const target = document.querySelector('.bean_container');
 
-const button = document.querySelector("#run-button");
-button.addEventListener("click", () => {
+const runButton = document.querySelector("#run-button");
+runButton.addEventListener("click", () => {
     getAllBeanVarieties()
         .then(beanVarieties => {
-            console.log(beanVarieties);
+            target.innerHTML = listBeanVarieties(beanVarieties);
         })
 });
 
-function getAllBeanVarieties() {
-    return fetch(url).then(resp => resp.json());
+const addBeanButton = document.querySelector("#add-bean-button");
+addBeanButton.addEventListener("click", () => {
+    
+})
+
+// Gets all Bean Varieties from the server
+const getAllBeanVarieties = () => fetch(url).then(resp => resp.json());
+
+// Separates the array of bean varieties and return a list of HTML
+const listBeanVarieties = (beanVarieties) => {
+    var beanString = "";
+    beanVarieties.forEach(bv => {
+        beanString += `
+        <div class="bean">
+            <div class="bean_name"><b>Name:</b> ${bv.name}</div>
+            <div class="bean_region"><b>Region:</b> ${bv.region}</div>
+            <div class="bean_note"><b>Note:</b> ${verifyNote(bv.notes)}</div>
+        </div>
+        `
+    })
+    return beanString;
+}
+
+const verifyNote = (note) => {
+    if(note !== null){
+        return note;
+    }
+    return "N/A";
 }
